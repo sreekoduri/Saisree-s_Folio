e7bc214142411a2179d42915f8efea2a8607fc15
SAISREE KODURI <sreekoduri21@gmail.com>
2026-05-28 17:48:05 +0530

    fixed react pdf issue

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
-import { FaLinkedinIn, FaFacebookF, FaGoogle } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn, FaGoogle } from "react-icons/fa";
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
           <h3>Designed and Developed by Saisree Koduri</h3>
         </Col>
+
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Saisree Koduri</h3>
         </Col>
+
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
+
             <li className="social-icons">
               <a
                 href="https://github.com/sreekoduri"
@@ -27,6 +31,7 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://www.linkedin.com/in/satyasree-koduri/"
@@ -37,10 +42,10 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-         
+
             <li className="social-icons">
               <a
-                href="mailto:sreekoduri21@gmail.com/"
+                href="mailto:sreekoduri21@gmail.com"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -48,6 +53,7 @@ function Footer() {
                 <FaGoogle />
               </a>
             </li>
+
           </ul>
         </Col>
       </Row>
--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -1,16 +1,13 @@
+import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
 import pdf from "../../assets/Saisree_Koduri_resume_2026.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import Zoom from "react-reveal/Zoom";
-import React, { useState, useEffect } from "react";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/Saisree-Koduri/Personal-Portfolio/main/src/assets/Saisree_Koduri_resume_2026.pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function Resume() {
   const [width, setWidth] = useState(1200);
@@ -24,34 +21,43 @@ function Resume() {
       <section className="home-section">
         <Container fluid id="home">
           <Particle />
+
           <Container className="home-content">
             <div
               className="d-flex justify-content-center"
               width="100%"
               style={{ backgroundColor: "#fbd9ad" }}
             >
-              <Zoom left cascade>
-                <h1 style={{ color: "rgb(134 61 176)" }}>RESUME</h1>
-              </Zoom>
+              <h1 style={{ color: "rgb(134 61 176)" }}>
+                RESUME
+              </h1>
             </div>
+
             <div fluid className="certificate-section" id="about">
+              
               <div className="d-flex justify-content-center mt-4">
                 <Button variant="primary" href={pdf} target="_blank">
                   <AiOutlineDownload />
                   &nbsp;Download Resume
                 </Button>
               </div>
+
               <div className="resume d-flex justify-content-center">
                 <Document file={pdf}>
-                  <Page pageNumber={1} scale={width > 786 ? 1.6 : 0.4} />
+                  <Page
+                    pageNumber={1}
+                    scale={width > 786 ? 1.6 : 0.4}
+                  />
                 </Document>
               </div>
+
               <div className="d-flex justify-content-center">
                 <Button variant="primary" href={pdf} target="_blank">
                   <AiOutlineDownload />
                   &nbsp;Download Resume
                 </Button>
               </div>
+
             </div>
           </Container>
         </Container>
