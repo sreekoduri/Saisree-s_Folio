cb7bb320491710b39dc15f2d5a757e724ac06654
SAISREE KODURI <sreekoduri21@gmail.com>
2026-05-28 18:55:45 +0530

    about page spaces fixing

--- src/components/aboutme/about/About.js
+++ src/components/aboutme/about/About.js
@@ -19,48 +19,48 @@ const About = () => {
 
           <Col md={7}>
             <p className="home-about-body">
-              My name is <b className="purple">Saisree Koduri </b>
-               and I am an{" "}
+              My name is <b className="purple">Saisree Koduri</b> and I am an{" "}
               <b className="purple">
                 Electronics and Communication Engineering
-              </b>
-                student at{" "}
+              </b>{" "}
+              student at{" "}
               <b className="purple">
                 Rajiv Gandhi University of Knowledge Technologies (RGUKT)
               </b>
               , Nuzvid, Andhra Pradesh.
-
+              
               <br />
               <br />
 
-              I am proficient in various programming languages such as  
+              I am proficient in various programming languages such as{" "}
               <b className="purple">
-                  Python, HTML, CSS, and JavaScript.
+                Python, HTML, CSS, and JavaScript.
               </b>
 
               <br />
               <br />
 
-              I also have experience working with frameworks and libraries such as
+              I also have experience working with frameworks and libraries such
+              as{" "}
               <b className="purple">
-                  NodeJS, ReactJS
+                NodeJS, ReactJS
               </b>
 
               <br />
               <br />
 
-              Whenever possible, I also apply my passion for developing products
-              with
+              Whenever possible, I also apply my passion for developing
+              products with{" "}
               <b className="purple">
-                   Modern Javascript Library and Frameworks.
+                Modern JavaScript Libraries and Frameworks.
               </b>
 
               <br />
               <br />
 
-              In my free time, I enjoy learning new technologies and
+              In my free time, I enjoy learning new technologies and{" "}
               <b className="purple">
-                   building new web technologies and products.
+                building new web technologies and products.
               </b>
             </p>
           </Col>
